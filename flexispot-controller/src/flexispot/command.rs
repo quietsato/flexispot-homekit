f1cd32dff289984c2a373864ac12e090017e0986
Taiki Sato <quietsato@gmail.com>
2024-05-22 22:03:47 +0900

    wip: feat: impl flexispot commands and queries

---
+++ flexispot-controller/src/flexispot/command.rs
@@ -0,0 +1,83 @@
+use std::{
+    borrow::BorrowMut,
+    error::Error,
+    fmt::Display,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+#[rustfmt::skip]
+mod constants {
+    pub const COMMAND_WAKEUP:  &[u8] = &[0x9b, 0x06, 0x02, 0x00, 0x00, 0x6c, 0xa1, 0x9d];
+    pub const COMMAND_UP:      &[u8] = &[0x9b, 0x06, 0x02, 0x01, 0x00, 0xfc, 0xa0, 0x9d];
+    pub const COMMAND_DOWN:    &[u8] = &[0x9b, 0x06, 0x02, 0x02, 0x00, 0x0c, 0xa0, 0x9d];
+    pub const COMMAND_M:       &[u8] = &[0x9b, 0x06, 0x02, 0x20, 0x00, 0xac, 0xb8, 0x9d];
+    pub const COMMAND_PRESET1: &[u8] = &[0x9b, 0x06, 0x02, 0x04, 0x00, 0xac, 0xa3, 0x9d];
+    pub const COMMAND_PRESET2: &[u8] = &[0x9b, 0x06, 0x02, 0x08, 0x00, 0xac, 0xa6, 0x9d];
+    pub const COMMAND_PRESET3: &[u8] = &[0x9b, 0x06, 0x02, 0x10, 0x00, 0xac, 0xac, 0x9d];
+    pub const COMMAND_PRESET4: &[u8] = &[0x9b, 0x06, 0x02, 0x00, 0x01, 0xac, 0x60, 0x9d];
+}
+
+#[derive(Debug)]
+pub enum FlexispotCommand {
+    Wakeup,
+    Up,
+    Down,
+    M,
+    Preset1,
+    Preset2,
+    Preset3,
+    Preset4,
+}
+
+impl FlexispotCommand {
+    pub fn to_u8_array(&self) -> &'static [u8] {
+        match self {
+            Self::Wakeup => constants::COMMAND_WAKEUP,
+            Self::Up => constants::COMMAND_UP,
+            Self::Down => constants::COMMAND_DOWN,
+            Self::M => constants::COMMAND_M,
+            Self::Preset1 => constants::COMMAND_PRESET1,
+            Self::Preset2 => constants::COMMAND_PRESET2,
+            Self::Preset3 => constants::COMMAND_PRESET3,
+            Self::Preset4 => constants::COMMAND_PRESET4,
+        }
+    }
+}
+
+pub struct FlexispotCommandResponse {}
+
+#[derive(Debug)]
+pub struct FlexispotCommandError(pub String);
+impl Display for FlexispotCommandError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+impl Error for FlexispotCommandError {}
+
+#[derive(Debug)]
+pub struct FlexispotCommandExecutor {
+    uart: Arc<Mutex<rppal::uart::Uart>>,
+}
+
+impl FlexispotCommandExecutor {
+    pub fn new(uart: Arc<Mutex<rppal::uart::Uart>>) -> Self {
+        Self { uart }
+    }
+    pub fn execute(
+        &self,
+        command: FlexispotCommand,
+    ) -> Result<FlexispotCommandResponse, FlexispotCommandError> {
+        self.uart
+            .lock()
+            .unwrap()
+            .borrow_mut()
+            .write(&command.to_u8_array())
+            .map_err(|e| FlexispotCommandError(e.to_string()))?;
+        Ok(FlexispotCommandResponse {})
+    }
+    pub fn sleep(&self, dur: Duration) {
+        std::thread::sleep(dur)
+    }
+}
---
+++ flexispot-controller/src/flexispot/query.rs
@@ -0,0 +1,151 @@
+use std::{
+    borrow::BorrowMut,
+    error::Error,
+    fmt::Display,
+    sync::{Arc, Mutex},
+};
+
+use rppal::uart::Queue;
+
+#[rustfmt::skip]
+mod constant {
+    pub const DATA_PREFIX: u8 = 0x9b;
+    pub const DATA_SUFFIX: u8 = 0x9d;
+
+    pub const DIGIT_0: u8 = 0x3f;
+    pub const DIGIT_1: u8 = 0x06;
+    pub const DIGIT_2: u8 = 0x5b;
+    pub const DIGIT_3: u8 = 0x4f;
+    pub const DIGIT_4: u8 = 0x66;
+    pub const DIGIT_5: u8 = 0x6d;
+    pub const DIGIT_6: u8 = 0x7c;
+    pub const DIGIT_7: u8 = 0x07;
+    pub const DIGIT_8: u8 = 0x7f;
+    pub const DIGIT_9: u8 = 0x6f;
+    pub const DIGIT_BYTES: &[u8] = &[
+        DIGIT_0,
+        DIGIT_1,
+        DIGIT_2,
+        DIGIT_3,
+        DIGIT_4,
+        DIGIT_5,
+        DIGIT_6,
+        DIGIT_7,
+        DIGIT_8,
+        DIGIT_9,
+    ];
+
+    pub const MASK_DIGIT: u8 = 0x7f;
+    pub const MASK_DOT  : u8 = 0x80;
+}
+
+#[derive(Debug)]
+pub enum FlexispotQueryError {
+    DeviceIsSleeped,
+    InvalidDigitByte(u8),
+    InvalidQueryResult(String),
+    Unknown(String),
+}
+impl Display for FlexispotQueryError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+impl Error for FlexispotQueryError {}
+
+#[derive(Debug)]
+pub struct FlexispotQueryResult<'a>(&'a [u8]);
+
+impl<'a> FlexispotQueryResult<'a> {
+    pub fn new(
+        bytes_with_prefix_and_suffix: &'a [u8],
+    ) -> Result<FlexispotQueryResult, FlexispotQueryError> {
+        let pos_begin = bytes_with_prefix_and_suffix
+            .iter()
+            .position(|&b| b == constant::DATA_PREFIX)
+            .ok_or(FlexispotQueryError::InvalidQueryResult(format!(
+                "data prefix ({}) is not found",
+                constant::DATA_PREFIX
+            )))?;
+        let pos_end = bytes_with_prefix_and_suffix
+            .iter()
+            .position(|&b| b == constant::DATA_SUFFIX)
+            .ok_or(FlexispotQueryError::InvalidQueryResult(format!(
+                "data suffix ({}) is not found",
+                constant::DATA_SUFFIX,
+            )))?;
+
+        Ok(Self(
+            &bytes_with_prefix_and_suffix[(pos_begin + 1)..pos_end],
+        ))
+    }
+    pub fn parse(&'a self) -> Result<FlexispotCurrentHeight, FlexispotQueryError> {
+        FlexispotCurrentHeight::from_digit_bytes(&[self.0[0], self.0[1], self.0[2]])
+    }
+}
+
+#[derive(Debug)]
+pub struct FlexispotCurrentHeight(f32);
+
+impl FlexispotCurrentHeight {
+    fn digit_byte_to_u32(digit_byte: u8) -> Option<u32> {
+        constant::DIGIT_BYTES
+            .iter()
+            .position(|&d| d == (digit_byte & constant::MASK_DIGIT))
+            .map(|u| u as u32)
+    }
+    pub fn from_digit_bytes(digit_bytes: &[u8; 3]) -> Result<Self, FlexispotQueryError> {
+        if digit_bytes == &[0x00, 0x00, 0x00] {
+            return Err(FlexispotQueryError::DeviceIsSleeped);
+        }
+
+        let mut decoded: u32 = 0;
+        for &digit_byte in digit_bytes {
+            decoded *= 10;
+            decoded += Self::digit_byte_to_u32(digit_byte)
+                .ok_or(FlexispotQueryError::InvalidDigitByte(digit_byte))?;
+        }
+        let mut decoded: f32 = decoded as f32;
+
+        let contains_decimal_point = (digit_bytes[1] & constant::MASK_DOT) != 0x00;
+        if contains_decimal_point {
+            decoded /= 10.0;
+        }
+        Ok(Self(decoded))
+    }
+    pub fn to_u32(&self) -> u32 {
+        return self.0.max(0.0) as u32;
+    }
+    pub fn to_f32(&self) -> f32 {
+        return self.0;
+    }
+}
+
+#[derive(Debug)]
+pub struct FlexispotQueryProcessor {
+    uart: Arc<Mutex<rppal::uart::Uart>>,
+}
+
+impl FlexispotQueryProcessor {
+    pub fn new(uart: Arc<Mutex<rppal::uart::Uart>>) -> Self {
+        Self { uart }
+    }
+    pub fn flush(&self) -> Result<(), String> {
+        self.uart
+            .lock()
+            .unwrap()
+            .borrow_mut()
+            .flush(Queue::Input)
+            .map_err(|e| e.to_string())
+    }
+    pub fn read(&self, buf: &mut [u8]) -> Result<usize, String> {
+        let len = self
+            .uart
+            .lock()
+            .unwrap()
+            .borrow_mut()
+            .read(buf)
+            .map_err(|e| e.to_string())?;
+        Ok(len)
+    }
+}
